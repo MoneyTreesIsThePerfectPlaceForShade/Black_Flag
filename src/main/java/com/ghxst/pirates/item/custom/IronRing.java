6ed313a5e8e660457d0b6b1109cca00e4be3fcb0
MoneyTreesIsThePerfectPlaceForShade <MoneyTreesIsThePerfectPlaceForShade@mt.com>
2022-04-03 16:04:59 +0500

    Добавил тултип кольцу, который объясняет что с ним делать

--- src/main/java/com/ghxst/pirates/item/custom/IronRing.java
+++ src/main/java/com/ghxst/pirates/item/custom/IronRing.java
@@ -1,20 +1,41 @@
 package com.ghxst.pirates.item.custom;
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class IronRing extends Item {
 
     public IronRing(Properties pProperties) {
         super(pProperties);
     }
+
+    //  добавляем тултип
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if (Screen.hasShiftDown()){//если нажат шифт
+            pTooltipComponents.add(new TranslatableComponent("tooltip.pirates.iron_ring.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.pirates.iron_ring.tooltip"));
+        }
+    }
+
 
     // это кольцо помогает находить сундуки пиратов
 
